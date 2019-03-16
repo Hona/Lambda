fe28ca6fb0dbd474f39b8c2667de5714d55edf66
Luke Parker <lukemparker@outlook.com>
2019-03-16 15:23:12 +1000

    Add project files.

---
+++ LambdaUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
