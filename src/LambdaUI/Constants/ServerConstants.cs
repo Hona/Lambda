ef794905d53d6fbd50defc487983e6fc4e1b6e4d
Luke Parker <lukemparker@outlook.com>
2019-12-11 22:37:56 +1000

    Changed the minecraft server IP to reflect the provider change

--- src/LambdaUI/Constants/ServerConstants.cs
+++ src/LambdaUI/Constants/ServerConstants.cs
@@ -2,7 +2,7 @@
 {
     internal static class ServerConstants
     {
-        internal const string MinecraftServerIpAddress = "139.99.168.78";
+        internal const string MinecraftServerIpAddress = "104.156.233.241";
         internal const string JustJumpServerIpAddress = "146.185.214.22";
         internal const string HightowerServerIpAddress = "146.185.214.22";
         internal const string JumpAcademyServerIpAddress = "146.185.214.22";
